e8c7af4019d061d0419c124ff309a1ec30d457c6
Erik <erik@elfga.com>
2012-03-05 11:06:18 -0500

    update copyright

--- src/ftp.c
+++ src/ftp.c
@@ -1,7 +1,7 @@
 
 /*****************************************************************************
  * vp    -    SDL based image viewer for linux and fbsd. (X and console)     *
- * Copyright (C) 2001-2010 Erik Greenwald <erik@smluc.org>                   *
+ * Copyright (C) 2001-2012 Erik Greenwald <erik@smluc.org>                   *
  *                                                                           *
  * This program is free software; you can redistribute it and/or modify      *
  * it under the terms of the GNU General Public License as published by      *
